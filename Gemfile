e1c738fc4877addd85b4d4adeee92b4473db0b52
Vieenay <vieenaysiingh@gmail.com>
2018-06-09 01:48:43 +0530

    added project controller

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootstrap-sass', '~> 3.3.7'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rubycritic'
+  gem 'reek'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(source)
+    # if source.is_a?(User)
+    #   return rep_root_path if( source.is?(:admin) || source.is?(:agent) )
+    #   return admin_root_path if source.is?(:super_admin)
+    # else
+    #   return rep_root_path
+    # end
+    root_path
+  end
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  update_attrs = [:password, :password_confirmation, :current_password]
+  devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+  end
 end
